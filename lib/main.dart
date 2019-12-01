bf296198e454be662c9c0d7e1db068a8027f0afa
LongKhoa <tailieuhay0706@gmail.com>
2019-12-01 08:03:22 +0700

    finish simple UI Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:ui' as prefix0;
+
+import 'package:circular_check_box/circular_check_box.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
@@ -12,15 +16,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: Login(),
@@ -34,102 +29,206 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool _value = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Image.asset(
-              'login.png',
-              fit: BoxFit.cover,
+      backgroundColor: Colors.white,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            'login.png',
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+          Container(
+              height: double.infinity,
               width: double.infinity,
+              color: Colors.black54),
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 100.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: RichText(
+                    maxLines: 4,
+                    text: TextSpan(children: [
+                      TextSpan(
+                          text: "FACI",
+                          style: TextStyle(
+                              fontSize: 50.0, fontWeight: FontWeight.bold)),
+                      TextSpan(
+                          text: "O",
+                          style: TextStyle(
+                              color: Colors.greenAccent,
+                              fontSize: 50.0,
+                              fontWeight: FontWeight.bold)),
+                    ]),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                  child: TextField(
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1.0,
+                          style: BorderStyle.solid,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 1.0,
+                            color: Colors.red,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(6.0))),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      hintText: "Password",
+                      hintStyle: TextStyle(color: Colors.white),
+                      prefixIcon: Icon(
+                        Icons.lock_outline,
+                        color: Colors.white,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1.0,
+                          style: BorderStyle.solid,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1.0,
+                          style: BorderStyle.solid,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            _value = !_value;
+                          });
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: _value
+                              ? Icon(
+                                  Icons.check,
+                                  size: 30.0,
+                                  color: Color(0xFF64DD17),
+                                )
+                              : Icon(
+                                  Icons.check_box_outline_blank,
+                                  size: 30.0,
+                                  color: Color(0xFF64DD17),
+                                ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          "Keep me logged in.",
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              letterSpacing: 1.2,
+                              color: Color(0xFFEEEEEE)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 50.0,
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      child: RaisedButton(
+                        color: Color.fromRGBO(40, 216, 161, 0.95),
+                        onPressed: () {},
+                        child: Text(
+                          "LOGIN",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Forgot password?",
+                        style: TextStyle(color: Colors.grey, fontSize: 16.0),
+                      ),
+                      Text(
+                        "Recover here",
+                        style: TextStyle(color: Colors.green, fontSize: 16.0),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 200.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Don't have an account?",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      Text(
+                        "Signup here",
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-                height: double.infinity,
-                width: double.infinity,
-                color: Colors.black54),
-
-           Column(
-             children: <Widget>[
-               SizedBox(
-                 height: 100.0,
-               ),
-               Padding(
-                 padding: EdgeInsets.all(10.0),
-                 child: Text(
-                   "FACIO",
-                   textAlign: TextAlign.center,
-                   style: TextStyle(
-                       fontSize: 60.0,
-                       color: Colors.white,
-                       fontWeight: FontWeight.bold),
-                 ),
-               ),
-               Padding(
-                 padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
-                 child: TextField(
-                   style: TextStyle(color: Colors.white,),
-                   decoration: InputDecoration(
-                     labelText: "Email",
-                     prefixIcon: Icon(Icons.email,color: Colors.white,),
-                     enabledBorder:OutlineInputBorder(
-                       borderSide: BorderSide(
-                         width: 1.0,
-                         style: BorderStyle.solid,
-                         color: Colors.grey,
-                       ),
-                     ) ,
-                     border: OutlineInputBorder(
-                       borderSide: BorderSide(
-                         width: 1.0,
-                         color: Colors.red,
-                       ),
-                         borderRadius:
-                         BorderRadius.all(Radius.circular(6.0))
-                     ),
-                   ),
-                 ),
-               ),
-               Padding(
-                 padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                 child: TextField(
-                   style: TextStyle(color: Colors.white),
-                   decoration: InputDecoration(
-                     labelText: "Password",
-                     hintText: "Password",
-                     hintStyle: TextStyle(color: Colors.white) ,
-                     prefixIcon: Icon(Icons.lock_outline,color: Colors.white,),
-                     enabledBorder:OutlineInputBorder(
-                       borderSide: BorderSide(
-                         width: 1.0,
-                         style: BorderStyle.solid,
-                         color: Colors.grey,
-                       ),
-                     ) ,
-                     border: OutlineInputBorder(
-                       borderSide: BorderSide(
-                         width: 1.0,
-                         style: BorderStyle.solid,
-                         color: Colors.grey,
-                       ),
-                     ) ,
-                   ),
-                 ),
-               ),
-               Container(
-                 height: 50.0,
-                 width: 50.0,
-
-                 decoration: BoxDecoration(
-                 shape: BoxShape.circle,
-
-                 ),
-               )
-             ],
-           ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
